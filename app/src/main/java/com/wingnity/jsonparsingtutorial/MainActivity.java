b8d3f79531e7a6e6b446919f487f94bc9b61a51c
Michaelg <shsmichael@gmail.com>
2016-09-08 13:44:21 +0300

    Initial commit

---
+++ app/src/main/java/com/wingnity/jsonparsingtutorial/MainActivity.java
@@ -0,0 +1,139 @@
+package com.wingnity.jsonparsingtutorial;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.net.ParseException;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+	
+	ArrayList<Book> bookList;
+	
+	bookAdapter adapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		bookList = new ArrayList<Book>();
+        new JSONAsyncTask().execute("http://ec2-52-43-108-148.us-west-2.compute.amazonaws.com:8080/useraccount/search/dosearchbytitle?userid=123123&title=me&fromyear=1960&toyear=1970");
+		//new JSONAsyncTask().execute("http://52.29.110.203:8080/LibArab/search/booktitle?userId=23&title=any");
+		  //http://ec2-52-43-108-148.us-west-2.compute.amazonaws.com:8080/useraccount/search/dosearchbytitle?userid=123123&title=me&fromyear=1960&toyear=1970
+		ListView listview = (ListView)findViewById(R.id.list);
+		adapter = new bookAdapter(getApplicationContext(), R.layout.row, bookList);
+		
+		listview.setAdapter(adapter);
+		
+		listview.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+					long id) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplicationContext(), bookList.get(position).getTitle(), Toast.LENGTH_LONG).show();
+			}
+		});
+	}
+
+
+	class JSONAsyncTask extends AsyncTask<String, Void, Boolean> {
+		
+		ProgressDialog dialog;
+		
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			dialog = new ProgressDialog(MainActivity.this);
+			dialog.setMessage("Loading, please wait");
+			dialog.setTitle("Connecting server");
+			dialog.show();
+			dialog.setCancelable(false);
+		}
+		
+		@Override
+		protected Boolean doInBackground(String... urls) {
+			try {
+				
+				//------------------>>
+				HttpGet httppost = new HttpGet(urls[0]);
+				HttpClient httpclient = new DefaultHttpClient();
+				HttpResponse response = httpclient.execute(httppost);
+
+				// StatusLine stat = response.getStatusLine();
+				int status = response.getStatusLine().getStatusCode();
+
+				if (status == 200) {
+					HttpEntity entity = response.getEntity();
+					String data = EntityUtils.toString(entity);
+					
+				
+					JSONObject jsono = new JSONObject(data);
+					JSONArray jarray = jsono.getJSONArray("list");
+					
+					for (int i = 0; i < jarray.length(); i++) {
+						JSONObject object = jarray.getJSONObject(i);
+					
+						Book currentbook = new Book();
+
+						currentbook.setRecordid(object.getString("recordId"));
+
+						currentbook.setTitle(object.getString("title"));
+					//	currentbook.setDescription(object.getString("description"));
+						currentbook.setCreationdate(object.getString("creationdate"));
+						currentbook.setPublisher(object.getString("publisher"));
+						currentbook.setAuthor(object.getString("author"));
+						//currentbook.setSpouse(object.getString("spouse"));
+					//	currentbook.setChildren(object.getString("children"));
+						//currentbook.setImage(object.getString("image"));
+
+						bookList.add(currentbook);
+					}
+					return true;
+				}
+				
+				//------------------>>
+				
+			} catch (ParseException e1) {
+				e1.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			return false;
+		}
+		
+		protected void onPostExecute(Boolean result) {
+			dialog.cancel();
+			adapter.notifyDataSetChanged();
+			if(result == false)
+				Toast.makeText(getApplicationContext(), "Unable to fetch data from server", Toast.LENGTH_LONG).show();
+
+		}
+	}
+	
+	
+
+	
+	
+	
+}
